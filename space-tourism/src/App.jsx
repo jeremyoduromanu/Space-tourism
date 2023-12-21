267a18b1edddbfb84e42ba6636e2f18f61914675
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-21 12:28:57 -0500

    Add background image to home page

--- space-tourism/src/App.jsx
+++ space-tourism/src/App.jsx
@@ -1,14 +1,20 @@
-
-import './App.css'
+import './App.css';
+import backgroundImage from './assets/home/background-home-desktop.jpg';
 
 function App() {
- 
+  const style = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    width: '100vw'
+  };
 
   return (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-  )
+    <div style={style}>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
